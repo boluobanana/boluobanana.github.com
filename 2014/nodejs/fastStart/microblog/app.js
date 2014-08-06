3d3b888d4747b114ce3c87eab700d189beea44bc
Too <=>
2014-08-06 12:11:33 +0800

    视图模板

--- 2014/nodejs/fastStart/microblog/app.js
+++ 2014/nodejs/fastStart/microblog/app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , util = require('util');
 
 var app = module.exports = express.createServer();
 
@@ -31,13 +32,49 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.get('/hello', routes.hello);
-app.all('/user/:username', function  (req, res, next) {
-  console.log('all methods captured');
-});
-app.get('/user/:username', function  (req, res) {
-  res.send('user: ' + req.params.username);
+app.get('/list', function  (req, res) {
+  res.render('list', {
+    title: 'List',
+    items: [1991, 'byvoid', 'express', 'Node.js']
+  });
 });
 
+app.helpers({
+  inspect: function(obj){
+    return util.inspect(obj, true);
+  }
+});
+app.dynamicHelpers({
+  headers: function  (req, res) {
+    return req.headers;
+  }
+});
+app.get('/helper', function  (req, res) {
+  res.render('helper', {
+    title: 'Helpers'
+  });
+});
+
+var users = {
+  'byvoid': {
+    name: 'Carbo',
+    website: 'http://www.byvoid.com'
+  }
+};
+app.all('/user/:username', function  (req, res, next) {
+  if (users[req.params.username]) {
+    next();
+  } else{
+    next(new Error(req.params.username + ' does not exist .'));
+  };
+});
+
+app.get('/user/:username', function  (req, res) {
+  res.send(JSON.stringify(users[req.params.username]));
+});
+app.put('/user/:username', function  (req, res) {
+  res.send('Done');
+})
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
