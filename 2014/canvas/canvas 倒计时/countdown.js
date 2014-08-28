8f6f3d315d67a355a064cdffbb778d3e87544a54
Tao <651549530@qq.com>
2014-08-28 20:33:36 +0800

    canvas学习1

---
+++ 2014/canvas/canvas 倒计时/countdown.js
@@ -0,0 +1,37 @@
+var WINDOW_WIDTH = 1024;
+var WINDOW_HEIGHT = 768;
+var RADIUS = 8;
+
+window.onload = function () {
+	var canvas = document.getElementById('canvas');
+
+	canvas.width = WINDOW_WIDTH;
+	canvas.height = WINDOW_HEIGHT;
+
+	var context = canvas.getContext('2d');
+	render(context);
+}
+
+function render (cxt) {
+	var hours = 12;
+	var minutes = 34;
+	var seconds = 56;
+
+	renderDigit(0,0,parseInt(hours/10), cxt);
+}
+
+function renderDigit (x,y,num,cxt) {
+
+	cxt.fillStyle = 'rgb(0,102,153)';
+
+	for (var i = 0; i < digit[num].length; i++) {
+		for (var j = 0; j < digit[num][i].length; j++) {
+			if (digit[num][i][j] == 1) {
+				cxt.beginPath();
+				cxt.arc(x+j*2*RADIUS+RADIUS,y+i*2*RADIUS+RADIUS,RADIUS,0,2*Math.PI );
+				cxt.closePath();
+				cxt.fill();
+			};
+		};
+	};
+}
