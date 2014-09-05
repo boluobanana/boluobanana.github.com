5904c70c54e003d1ae7c81c35dad7471e223a09c
Tao <651549530@qq.com>
2014-09-05 19:29:48 +0800

    canvas小球动画

--- 2014/canvas/canvas 倒计时/countdown.js
+++ 2014/canvas/canvas 倒计时/countdown.js
@@ -1,27 +1,75 @@
 var WINDOW_WIDTH = 1024;
 var WINDOW_HEIGHT = 768;
 var RADIUS = 8;
+var MARGIN_TOP = 60;
+var MARGIN_LEFT = 30;
+
+const endTime = new Date(2014,8,5,18,00,00);
+var curShowTimeSeconds = 0;
 
 window.onload = function () {
 	var canvas = document.getElementById('canvas');
+	var context = canvas.getContext('2d');
 
 	canvas.width = WINDOW_WIDTH;
 	canvas.height = WINDOW_HEIGHT;
 
-	var context = canvas.getContext('2d');
-	render(context);
+	curShowTimeSeconds = getCurrentShowTimeSeconds();
+
+	setInterval(
+		function () {
+			render(context);
+			update();
+			
+		},
+		50
+	);
+}
+
+function getCurrentShowTimeSeconds(){
+
+	var curTime = new Date();
+	var ret = endTime.getTime() - curTime.getTime();
+	ret = Math.round(ret/1000);
+
+	return ret >= 0 ? ret : 0;
+}
+
+function update () {
+	
+	var nextShowTimeSeconds = getCurrentShowTimeSeconds();
+
+	var nextHours = parseInt(curShowTimeSeconds/3600);
+	var nextMinutes = parseInt((curShowTimeSeconds - nextHours * 3600)/60);
+	var nextSeconds = parseInt(curShowTimeSeconds % 60);
+
+	var curHours = parseInt(curShowTimeSeconds/3600);
+	var curMinutes = parseInt((curShowTimeSeconds - curHours * 3600)/60);
+	var curSeconds = parseInt(curShowTimeSeconds % 60);
+
+	if (nextSeconds != curShowTimeSeconds) {
+		curShowTimeSeconds = nextShowTimeSeconds;
+	};
 }
 
 function render (cxt) {
-	var hours = 12;
-	var minutes = 34;
-	var seconds = 56;
 
+	cxt.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
+
+	var hours = parseInt(curShowTimeSeconds/3600);
+	var minutes = parseInt((curShowTimeSeconds - hours * 3600)/60);
+	var seconds = parseInt(curShowTimeSeconds % 60);
+
+
+	renderDigit(MARGIN_LEFT,MARGIN_TOP,parseInt(hours/10), cxt);
+	renderDigit(MARGIN_LEFT + 15*(RADIUS+1),MARGIN_TOP,parseInt(hours%10),cxt);
+	renderDigit(MARGIN_LEFT + 30*(RADIUS+1),MARGIN_TOP,10,cxt);
+	renderDigit(MARGIN_LEFT + 39*(RADIUS+1),MARGIN_TOP,parseInt(minutes/10), cxt);
+	renderDigit(MARGIN_LEFT + 54*(RADIUS+1),MARGIN_TOP,parseInt(minutes%10),cxt);
+	renderDigit(MARGIN_LEFT + 69*(RADIUS+1),MARGIN_TOP,10,cxt);
+	renderDigit(MARGIN_LEFT + 78*(RADIUS+1),MARGIN_TOP,parseInt(seconds/10), cxt);
+	renderDigit(MARGIN_LEFT + 93*(RADIUS+1),MARGIN_TOP,parseInt(seconds%10),cxt);
 	
-
-	renderDigit(0,0,parseInt(hours/10), cxt);
-	renderDigit(0,0,parseInt(minutes/10), cxt);
-	renderDigit(0,0,parseInt(seconds/10), cxt);
 }
 
 function renderDigit (x,y,num,cxt) {
@@ -39,3 +87,4 @@ function renderDigit (x,y,num,cxt) {
 		};
 	};
 }
+
