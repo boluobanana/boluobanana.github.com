3173b1da2281c499add28f222be6d12ab3c7465e
Tao <651549530@qq.com>
2014-10-24 18:53:13 +0800

    搭积木样式完成

--- 2014/css3/transform01.js
+++ 2014/css3/transform01.js
@@ -4,4 +4,6 @@ function route () {
 	var valueZ = document.getElementById('tZ').value;
 
 	document.getElementById('block').style.webkitTransform = "rotateX("+valueX+"deg) rotateY("+valueY+"deg) rotateZ("+valueZ+"deg)";
+	document.getElementById('block').style.transform = "rotateX("+valueX+"deg) rotateY("+valueY+"deg) rotateZ("+valueZ+"deg)";
+	document.getElementById('block1').style.transform = "rotateX("+valueX+"deg) rotateY("+valueY+"deg) rotateZ("+valueZ+"deg)";
 }
