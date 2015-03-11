b90ed8ef795db78b456f14cb58ed92844187e390
Tao <651549530@qq.com>
2015-03-11 16:41:46 +0800

    新学期

---
+++ 2015/demo/跨浏览器取得窗口左边和上边位置.js
@@ -0,0 +1,4 @@
+var leftPos = (typeof window.screenLeft == "number") ? 
+				window.screenLeft : window.screenX;
+var topPos = (typeof window.screenTop == "number") ?
+ 				window.screenTop : window.screenY;
