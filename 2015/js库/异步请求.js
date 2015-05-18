6b119b62f1acc6abdf9252c6abb7f1499ba90a95
Tao <651549530@qq.com>
2015-05-18 15:46:27 +0800

    ocrad.js

--- 2015/js库/异步请求.js
+++ 2015/js库/异步请求.js
@@ -1,3 +1,25 @@
+function createXHR(){
+		if (typeof XMLHttpRequest != "undefined") {
+			return new XMLHttpRequest();
+		} else if (typeof ActiveXobject != "undefined") {
+			if (typeof arguments.callee.activeString != "string") {
+				var versions = ["MSXML2.XMLHttp.6.0", "XSXML2.XMLHttp.3.0", "MSXML2.XMLHttp"],i,len;
+				for (i=0, len=versions.length; i < len; i++){
+					try{
+						new AcctiveXobject(versions[i]);
+						arguments.callee.activeXString = versions[i];
+						break;
+					}catch(ex){
+
+					}
+				}
+			};
+			return new ActiveXobject(arguments.callee.activeXString);
+		}else{
+			throw new Error("no XHR object available.")
+		}
+	}
+	
 var xhr = createXHR();
 xhr.onreadystatechange = function(){
 	if(xhr.readyState == 4){
